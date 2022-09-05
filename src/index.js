e90f3a91e5a3908be4811d8a6c018cbc92ebeba0
Leo <leomutungi19@gmail.com>
2022-09-05 23:47:00 +0300

    completed assignement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // write your code here
+const baseUrl = "http://localhost:3000/ramens";
+const ramenMenu = document.querySelector("#ramen-menu");
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM loaded");
+});
+function displayRamen() {
+  fetch(baseUrl)
+    .then((res) => res.json())
+    .then(getAllRamen);
+}
+
+function getAllRamen(ramenArr) {
+  ramenArr.forEach(getRamen);
+}
+function getRamen(ramenObj) {
+  const ramenImg = document.createElement("img");
+  ramenImg.src = ramenObj.image;
+  ramenMenu.append(ramenImg);
+}
